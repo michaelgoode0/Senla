b027c46f89497cd6ceee50d13efddb15798460cd
Pepega <burtsegalya@mail.ru>
2021-03-24 19:31:34 +0300

    Initial commit

--- src/com/company/entities/IBouquet.java
+++
@@ -1,9 +0,0 @@
-package com.company.entities;
-
-import java.util.ArrayList;
-
-public interface IBouquet {
-    double getCost();
-    void add(Flower flower);
-    void showFlowers();
-}
--- src/com/company/enums/TypeOfFlowers.java
+++
@@ -1,8 +0,0 @@
-package com.company.enums;
-
-public enum TypeOfFlowers {
-    WhiteFlower,
-    PurpleFlower,
-    RedFlower,
-    YellowFlower
-}
